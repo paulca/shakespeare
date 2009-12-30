697b9cde24f89fe8f9f03e908ac1199583e6b0c0
Paul Campbell <paul@Paul-Campbells-MacBook-Pro.local>
2009-12-30 07:08:05 +0000

    include page_content as a helper method

--- lib/helpers/shakespeare_helper.rb
+++ lib/helpers/shakespeare_helper.rb
@@ -1,5 +1,9 @@
 module ShakespeareHelper
   
+  def self.included(base)
+    base.send :helper_method, :page_content
+  end
+  
   def page_content
     @page_content = Page.find_by_url("#{controller_name}/#{action_name}")
   end
