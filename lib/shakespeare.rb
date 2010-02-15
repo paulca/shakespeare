2029dc02b9e353aaec4483ac9fd61abf05ceefa3
Paul Campbell <paul@rslw.com>
2010-02-15 14:23:56 +0000

    make sure to include view helpers

--- lib/shakespeare.rb
+++ lib/shakespeare.rb
@@ -4,4 +4,5 @@ require 'shakespeare/helpers'
 require 'shakespeare/view_helpers'
 
 ApplicationController.send(:include, Shakespeare::Helpers)
+ActionView::Base.send(:include, Shakespeare::Helpers)
 ActionView::Base.send(:include, Shakespeare::ViewHelpers)
